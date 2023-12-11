7e2da15028eba6ad1343f929a7c86990163a7798
Thiago <thiago.romaris@sptech.school>
2023-12-11 04:10:10 -0300

    Add font and search functionality to Marvel component

--- src/app/marvel/marvel.component.ts
+++ src/app/marvel/marvel.component.ts
@@ -3,14 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-marvel',
   templateUrl: './marvel.component.html',
-  styleUrls: ['./marvel.component.css']
+  styleUrls: ['./marvel.component.css'],
+  providers: []
 })
 export class MarvelComponent implements OnInit {
+  nomeHeroi: string;
+  loading: boolean = false;
 
-  constructor() { }
+  constructor(
+  ) { }
 
   ngOnInit() {
   }
 
-  loading: boolean = false;
+  buscarHeroi() {
+      this.loading = true;
+  }
 }
--- src/app/table-marvel/table-marvel.component.ts
+++ src/app/table-marvel/table-marvel.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { MarvelService } from '../services/marvel.service';
 import { DadosMarvel, MarvelPagination, DadosMarvelHerois } from '../services/marvel-result.service';
+import { MarvelComponent } from '../marvel/marvel.component';
 
 @Component({
   selector: 'app-table-marvel',
@@ -12,11 +13,12 @@ export class TableMarvelComponent implements OnInit {
 
   constructor(
     private marvelService: MarvelService,
+    private marvelComponent: MarvelComponent
   ) { }
 
   nomeHeroi: string;
-
   ngOnInit() {
+     this.nomeHeroi = this.marvelComponent.nomeHeroi;
     if (this.nomeHeroi) {
       this.buscarHeroi(this.nomeHeroi);
     }
@@ -37,36 +39,65 @@ export class TableMarvelComponent implements OnInit {
     this.key = key;
     this.reverse = !this.reverse;
   }
-  
 
-  buscarHeroi(nomeHeroi: string){
+
+  buscarHeroi(nomeHeroi: string) {
     this.marvelService.getCharacters(nomeHeroi)
-    .subscribe(
-      (next: any) => {
-        this.marvelPagination = next
-        const dados = JSON.parse(JSON.stringify(this.marvelPagination.data));
-        const resultados = dados.results;
-  
-        if (resultados && resultados.length > 0) {
-          this.dadosMarvel = resultados;
-          console.log(this.dadosMarvel);
-        } else {
-          console.log('Nenhum resultado encontrado.');
+      .subscribe(
+        (next: any) => {
+          this.marvelPagination = next
+          const dados = JSON.parse(JSON.stringify(this.marvelPagination.data));
+          const resultados = dados.results;
+
+          if (resultados && resultados.length > 0) {
+            this.dadosMarvel = resultados;
+            console.log(this.dadosMarvel);
+          } else {
+            console.log('Nenhum resultado encontrado.');
+          }
+        },
+        (error: any) => {
+          console.error('Erro na requisição HTTP:', error);
         }
-      },
-      (error: any) => {
-        console.error('Erro na requisição HTTP:', error);
-      }
-    )
+      )
   }
 
-verDetalhesHeroi: boolean = false;
-mostrarDetalhes(i: number) {
-  let elemento = document.getElementById('collapseRow' + i);
-  elemento.classList.toggle('show');
-}
+  verDetalhesHeroi: boolean = false;
+  mostrarDetalhes(i: number) {
+    let elemento = document.getElementById('heroiShow' + i);
+    if(elemento.classList.contains('show')){
+      elemento.classList.remove('show');
+    }
+    else{
+      elemento.classList.add('show');
+    }
+  }
 
-  
+  formatarData(data: string): string {
+    const dataFormatada = new Date(data);
+    const dia = dataFormatada.getDate();
+    const mes = dataFormatada.getMonth() + 1;
+    const ano = dataFormatada.getFullYear();
+
+    const diaFormatado = dia < 10 ? '0' + dia : dia;
+    const mesFormatado = mes < 10 ? '0' + mes : mes;
+
+    const dataFinal = `${diaFormatado}/${mesFormatado}/${ano}`;
+
+    return dataFinal;
+  }
+
+  x: number;
+  onMouseDown(event: MouseEvent) {
+    this.x = event.clientX;
+  }
+
+  // Função para detectar o evento mousemove
+  onMouseMove(event: MouseEvent) {
+    const carrossel = document.getElementById('carrossel');
+    carrossel.scrollLeft += (event.clientX - this.x);
+    carrossel.style.scrollBehavior = 'smooth';
+  }
 
 
 }
